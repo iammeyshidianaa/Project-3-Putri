5d30787f889b0b6c6c3be7a82f73079d5cdb95c7
lailiaaliaa <maulidahlailia19@gmail.com>
2022-11-09 09:48:04 +0700

    pinjambarang cntrl

--- sarpras/app/Http/Controllers/PinjambarangController.php
+++ sarpras/app/Http/Controllers/PinjambarangController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Baranghabis;
-use App\Models\databarang;
 use App\Models\Pinjambarang;
 use Illuminate\Http\Request;
 use App\Models\peminjamanadmin;
@@ -14,56 +12,89 @@ class PinjambarangController extends Controller
     public function peminjamanadmin()
     {
         $peminjaman = peminjamanadmin::all();
+        $peminjaman = peminjamanadmin::where('status3', null)->get();
 
         return view('admin.permintaan_peminjaman.peminjaman', compact('peminjaman'));
     }
 
-    //guru barang tdk habis
-    public function pinjambarang_guru()
+    public function peminjamansiswa()
     {
-        $pinjambarang = Pinjambarang::all();
-        //nama barang
-        $namabarang = databarang::all();
-
-        return view('guru.peminjaman.pinjam_tdkhabis', compact('pinjambarang','namabarang'));
+        $peminjaman = peminjamanadmin::all();
+        $peminjaman = peminjamanadmin::where('status3', 'Dipinjam')->orwhere('status3', null)->get();
+        return view('siswa.barang_dipinjam.barangdipinjam', compact('peminjaman'));
     }
-
-    public function insertpinjam_guru(Request $request)
+    public function dafsis()
     {
-        $peminjaman = Pinjambarang::create($request->all());
-        return redirect('pinjambarang_guru')->with('success','Data Terkirim Ke Admin');
+        $peminjaman = peminjamanadmin::where('status3', 'Dipinjam')->get();
+        return view('admin.permintaan_peminjaman.peminjaman_siswa', compact('peminjaman'));
     }
-
-    //guru barang habis
-    public function barang_habis()
+    public function tolaks()
     {
-        $pinjambarang = Pinjambarang::all();
-        //nama barang
-        $namabarangs = Baranghabis::all();
-
-        return view('guru.peminjaman.pinjamhabis', compact('pinjambarang','namabarangs'));
+        $peminjaman = peminjamanadmin::where('status3', 'Ditolak')->orwhere('status3', 'Telah Dikembalikan')->get();
+        return view('siswa.riwayat.riwayat_tolak', compact('peminjaman'));
     }
-
-    public function insertpinjam_guru2(Request $request)
+    public function riwayatsw()
     {
-        $peminjaman = Pinjambarang::create($request->all());
-        return redirect('pinjamhabis')->with('success','Data Terkirim Ke Admin');
+        $peminjaman = peminjamanadmin::where('status3', 'Ditolak')->orwhere('status3', 'Telah Dikembalikan')->get();
+        return view('admin.daftar_riwayat.sedangpinjam', compact('peminjaman'));
+    }
+    public function updateStatus3($status, $id){
+        $data = peminjamanadmin::where('id', $id)->first();
+
+        if($status == 'terima'){
+            $data->update(['status3' => 'Dipinjam']);
+        }elseif($status == 'tolak'){
+            $data->update(['status3' => 'Ditolak']);
+        }else{
+            return redirect()->back();
+        }
+
+        return redirect('daftars');
+    }
+    public function pengembalianadmin()
+    {
+        $peminjaman = peminjamanadmin::where('status3', 'Menunggu Persetujuan')->get();
+        return view('siswa.permintaan_pengembalian.pengembalian', compact('peminjaman'));
+    }
+    public function pensis($id)
+    {
+        $peminjaman = peminjamanadmin::where('id',$id)->update(['status3' => 'Menunggu Persetujuan']);
+        return redirect('datapinjam')->with('message','Pesan berhasil dikirimkan');
+    }
+    public function kembalisis()
+    {
+        $pinjambarang = Pinjambarang::where('status3', 'Menunggu Persetujuan')->get();
+        return view('admin.permintaan_pengembalian.kembali_siswa', compact('pinjambarang'));
     }
 
     //siswa
     public function pinjambarang()
     {
         $pinjambarang = Pinjambarang::all();
-         // pinjam barang siswa
-         $namabarangsiswa = databarang::all();
 
-        return view('siswa.pinjambarang.pinjam', compact('pinjambarang','namabarangsiswa'));
+        return view('siswa.pinjambarang.pinjam', compact('pinjambarang'));
     }
 
     public function insertpinjamsiswa(Request $request)
     {
         $peminjaman = Pinjambarang::create($request->all());
-        return redirect('pinjambarang')->with('success','Data Terkirim Ke Admin');
+        return redirect('datapinjam')->with('success','Data Terkirim Ke Admin');
     }
+
+    public function updateStatus2($status, $id){
+        $data = peminjamanadmin::where('id', $id)->first();
+
+        if($status == 'terima'){
+            $data->update(['status3' => 'Telah Dikembalikan']);
+        }elseif($status == 'tolak'){
+            $data->update(['status3' => 'Ditolak']);
+        }else{
+            return redirect()->back();
+        }
+
+        return redirect('riwayatsw');
+    }
+
+
 
 }
