3d7dbccb56c2077d3091c00a2885379d0da2a078
lailiaaliaa <maulidahlailia19@gmail.com>
2022-11-30 08:21:33 +0700

    indxadmn

--- sarpras/resources/views/admin/index.blade.php
+++ sarpras/resources/views/admin/index.blade.php
@@ -35,7 +35,41 @@
                         </div>
                     </div>
                 </div>
-
+                </div>
+                <div class="col-md-6 grid-margin transparent">
+              <div class="row">
+                <div class="col-md-6 mb-4 stretch-card transparent">
+                  <div class="card card-tale">
+                    <div class="card-body">
+                      <p class="mb-4">Pengajuan Guru</p>
+                      <p class="fs-30 mb-2">{{ $permintaan_pengajuan }}</p>
+                      <p>semua data</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 mb-4 stretch-card transparent">
+                  <div class="card card-dark-blue">
+                    <div class="card-body">
+                      <p class="mb-4">Peminjaman Guru</p>
+                      <p class="fs-30 mb-2">{{ $permintaan_pengembalian_guru }}</p>
+                      <p>semua data</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6 mb-4 mb-lg-0 stretch-card transparent">
+                  <div class="card card-light-blue">
+                    <div class="card-body">
+                      <p class="mb-4">Peminjaman Siswa</p>
+                      <p class="fs-30 mb-2">{{ $permintaan_pinjam_siswa }}</p>
+                      <p>semua data</p>
+                    </div>
+                  </div>
+                </div>
+</div>
+            </div>
+                </div>
                 <div class="card">
                             <div class="card-body">
                                 <div id="grafik"></div>
@@ -44,6 +78,10 @@
                             </div>
                         </div><!-- /.row -->
                 </div>
+
+
+
+
 
     <!-- <div class="content-wrapper">
         <div class="content-header">
@@ -136,8 +174,8 @@
                     <canvas id="order-chart"></canvas>
                 </div>
             </div>-->
-        </div>
-    </div>
+
+
 
     <!-- content-wrapper ends -->
     <!-- partial:partials/_footer.html -->
